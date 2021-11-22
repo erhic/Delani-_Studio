6f507200567d3ed658d3aeba6e824c3cda5db5da
erhic <eric.gichovi@student.moringaschool.com>
2021-11-22 12:20:18 +0300

    creating a function, to validate the input

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let name = document.getElementById('text_area_name').value
-let email = document.getElementById('text_area_email').value
-if (name == "" || email == "") {
-    alert("fill all the fields")
+function validateImput() {
+    let name = document.getElementById('text_area_name').value
+    let email = document.getElementById('text_area_email').value
+    if (name == "" || email == "") {
+        alert("fill all the fields")
+    }
 }
