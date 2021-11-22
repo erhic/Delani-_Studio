e99616d37ab3bcaa8f47da38de6b18faf5a0f817
erhic <eric.gichovi@student.moringaschool.com>
2021-11-22 12:18:26 +0300

    declaring variables, the name and email to be able to set no empty input on click button

---
+++ script.js
@@ -0,0 +1,5 @@
+let name = document.getElementById('text_area_name').value
+let email = document.getElementById('text_area_email').value
+if (name == "" || email == "") {
+    alert("fill all the fields")
+}
