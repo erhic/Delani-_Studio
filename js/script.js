2949acfbb94e4f9e39230edbb86f6631b152d9b3
erhic <eric.gichovi@student.moringaschool.com>
2021-11-24 19:31:53 +0300

    add  an event lister, on click to toogle

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.getElementById('btn_submit').addEventListener('click', function() { validateImput(); })
+
+function validateImput() {
+    let name = document.getElementById('text_area_name').value
+    let email = document.getElementById('text_area_email').value
+    if (name === "" || email === "") {
+        alert("fill all the fields")
+    } else {
+
+    }
+}
+// const body = document.body
+// const titl = document.querySelector(".overlay")
+// titl.style.backGroundColor = "red"
+
+
+$(document).ready(function() {
+    $("#serviceimg1").click(function() {
+        $(".whatwedo_para").toggle("slow");
+        $("#serviceimg1").hide()
+    })
+})
+
+$(document).ready(function() {
+    $(".whatwedo_para").click(function() {
+        $("#serviceimg1").show()
+        $(".whatwedo_para").hide();
+    })
+
+})
